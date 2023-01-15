b05db95916eaa2228301585cef1b74ca7f7bef4d
Marveeen <jmvroqueee@gmail.com>
2023-01-15 10:58:32 +0800

    connected routes and view for page access

--- app.js
+++ app.js
@@ -7,10 +7,20 @@ const app = express()
 
 const PORT = '8080'
 
+const adminData = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/admin', adminData.routes)
+app.use(shopRoutes)
+
+app.use((req, res, next) => {
+    res.status(404).render('404', { pageTitle: 'PAGE NOT FOUND', path: ''})
+})
 
 app.listen(PORT, () => console.log('Server running'))
