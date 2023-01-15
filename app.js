96f34d5780793d71fd483ce29c89ef0c4e2c094f
Marveeen <jmvroqueee@gmail.com>
2023-01-15 10:39:31 +0800

    added public folder for styling

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 const express = require('express')
 const bodyParser = require('body-parser')
 
@@ -9,5 +11,6 @@ app.set('view engine', 'ejs')
 app.set('views', 'views')
 
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(PORT, () => console.log('Server running'))
