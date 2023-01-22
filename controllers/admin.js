4bceb65a68cdf57aa9a0bda2c7e4b77697485076
Marveeen <jmvroqueee@gmail.com>
2023-01-22 09:33:55 +0800

    added edit product page

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,9 +1,36 @@
 const Product = require("../models/product");
 
 const getAddProduct = (req, res, next) => {
-  res.render("admin/add-product", {
+  res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
+    editing: false
+  });
+};
+
+const postAddProduct = (req, res, next) => {
+  const { title, imageUrl, price, description } = req.body;
+
+  const product = new Product(title, imageUrl, description, price);
+  product.save();
+  res.redirect("/");
+};
+
+const getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+
+  const { productId } = req.params;
+  Product.findById(productId, (product) => {
+    if (!product) {
+      return res.redirect('/')
+    }
+
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: Boolean(editMode),
+      product,
+    });
   });
 };
 
@@ -17,16 +44,9 @@ const getAdminProducts = (req, res, next) => {
   });
 };
 
-const postAddProduct = (req, res, next) => {
-  const { title, imageUrl, price, description } = req.body;
-
-  const product = new Product(title, imageUrl, description, price);
-  product.save();
-  res.redirect("/");
-};
-
 module.exports = {
   getAddProduct,
   getAdminProducts,
   postAddProduct,
+  getEditProduct,
 };
--- routes/admin.js
+++ routes/admin.js
@@ -5,8 +5,11 @@ const adminController = require('../controllers/admin')
 
 router.get("/add-product", adminController.getAddProduct);
 
+router.get("/edit-product/:productId", adminController.getEditProduct);
+
 router.get("/products", adminController.getAdminProducts);
 
 router.post("/add-product", adminController.postAddProduct);
+
 
 module.exports = router;
