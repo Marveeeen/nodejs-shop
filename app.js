2e5cc0942611d5e5b195a8654a9ef9429f7df850
Marveeen <jmvroqueee@gmail.com>
2023-02-01 13:50:51 +0800

    refactor mongoConnect

--- app.js
+++ app.js
@@ -6,19 +6,12 @@ const bodyParser = require("body-parser");
 const PORT = "8080";
 
 const errorController = require("./controllers/error");
-const sequelize = require("./util/database");
-
-const Product = require("./models/product");
-const User = require("./models/user");
-const Cart = require("./models/cart");
-const CartItem = require("./models/cart-item");
-const Order = require("./models/order");
-const OrderItem = require("./models/order-item");
+const mongoConnect = require("./util/database").mongoConnect;
 
 const app = express();
 
 const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+// const shopRoutes = require("./routes/shop");
 
 // setup template engine
 app.set("view engine", "ejs");
@@ -30,60 +23,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // for static resources
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-  User.findByPk(1)
-    .then((user) => {
-      req.user = user;
-      next();
-    })
-    .catch((err) => console.log(err));
-});
+// app.use((req, res, next) => {});
 
 // for routing
 app.use("/admin", adminRoutes);
-app.use(shopRoutes);
+// app.use(shopRoutes);
 
-app.use(errorController.get404);
+// app.use(errorController.get404);
 
-// Defining relations
-Product.belongsTo(User, {
-  constraints: true,
-  onDelete: "CASCADE",
+mongoConnect(() => {
+  app.listen(PORT);
 });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-User.hasMany(Order);
-Order.belongsTo(User);
-Order.belongsToMany(Product, { through: OrderItem });
-Product.belongsToMany(Order, { through: OrderItem });
-
-sequelize
-  // .sync({force: true})
-  .sync()
-  .then((result) => {
-    return User.findByPk(1);
-  })
-  .then((user) => {
-    if (!user) {
-      return User.create({ name: "Marvin", email: "test@test.com" });
-    }
-
-    return Promise.resolve(user);
-  })
-  .then((user) => {
-    console.log("USER FOUND");
-    // SERVE SERVER 8080
-    if (user.getCart()) {
-      return Promise.resolve();
-    }
-    return user.createCart();
-  })
-  .then((cart) => {
-    app.listen(PORT, () => console.log("Server running"));
-  })
-  .catch((err) => {
-    console.log(err);
-  });
--- util/database.js
+++ util/database.js
@@ -1,5 +1,32 @@
 const mongodb = require("mongodb");
 const { MongoClient } = mongodb;
 
-MongoClient.connect('mongodb+srv://marveeen:<password>@cluster0.tpetww6.mongodb.net/?retryWrites=true&w=majority');
+let _database;
 
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://marveeen:WjchIjFvgdOUeBJk@cluster0.tpetww6.mongodb.net/?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("Connected!");
+      _database = client.db();
+      callback();
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDatabase = () => {
+  if (!_database) {
+    throw "No database found!";
+  }
+
+  return _database;
+};
+
+module.exports = {
+  mongoConnect,
+  getDatabase,
+};
