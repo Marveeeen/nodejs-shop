ed7121c932f54c6104c6931a7829b33c3ed5964e
Marveeen <jmvroqueee@gmail.com>
2023-02-01 13:03:34 +0800

    convert database to mongodb

---
+++ util/database.js
@@ -0,0 +1,5 @@
+const mongodb = require("mongodb");
+const { MongoClient } = mongodb;
+
+MongoClient.connect('mongodb+srv://marveeen:<password>@cluster0.tpetww6.mongodb.net/?retryWrites=true&w=majority');
+
